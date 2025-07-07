d6f3ef7c7074f91850ec6ba2f257d84b7b466c65
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2025-07-07 09:35:41 +0300

    Restructure repository to retain frontend directory only

--- setup_mysql_dev.sql
+++
@@ -1,8 +0,0 @@
--- script prepares a mysql server for the project
-
-CREATE DATABASE IF NOT EXISTS hms_dev_db;
-CREATE USER IF NOT EXISTS "hms_dev"@"localhost" IDENTIFIED BY "hms_dev_pwd";
-GRANT ALL PRIVILEGES ON hms_dev_db.* TO "hms_dev"@"localhost";
-GRANT SELECT ON performance_schema.* TO "hms_dev"@"localhost";
-
-FLUSH PRIVILEGES;
