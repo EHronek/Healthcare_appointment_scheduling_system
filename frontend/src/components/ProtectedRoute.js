819df63c5b498a609c13c69c3703255c951814a6
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2025-07-09 15:55:50 +0300

    Routes

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -4,9 +4,8 @@ import { Navigate } from 'react-router-dom';
 function ProtectedRoute({ children, allowedRole }) {
   const user = JSON.parse(localStorage.getItem('user'));
 
-  // If no user or role mismatch, redirect to login
   if (!user || user.role !== allowedRole) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/login" replace />;
   }
 
   return children;
